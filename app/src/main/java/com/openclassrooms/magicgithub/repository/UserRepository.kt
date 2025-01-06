616fcce744a713cd7dedd066f98c149fc2b2ed75
Lafaire Dylan <dylan.lafaire.ts2@gmail.com>
2025-01-06 11:48:17 +0100

    Etape 1

--- app/src/main/java/com/openclassrooms/magicgithub/repository/UserRepository.kt
+++ app/src/main/java/com/openclassrooms/magicgithub/repository/UserRepository.kt
@@ -3,18 +3,17 @@ package com.openclassrooms.magicgithub.repository
 import com.openclassrooms.magicgithub.api.ApiService
 import com.openclassrooms.magicgithub.model.User
 
-class UserRepository(
-    private val apiService: ApiService
-) {
+class UserRepository(private val apiService: ApiService) {
+
     fun getUsers(): List<User> {
-        TODO("Should return the list of users retrieved from the api service")
+        return apiService.getUsers()
     }
 
     fun addRandomUser() {
-        TODO("Should add a random user")
+        apiService.addRandomUser()
     }
 
     fun deleteUser(user: User) {
-        TODO("Should remove the user")
+        apiService.deleteUser(user)
     }
 }
